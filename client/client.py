7b76359e333ca2b15e811744e2df41f253552039
Rajamohan J <71580990+rjabbala50@users.noreply.github.com>
2022-09-30 03:22:42 +0530

    working code.

--- client/client.py
+++ client/client.py
@@ -1,21 +1,31 @@
 import socket
+import sys
 import time
 
+PATH = "/Users/rjabbala/Projects /Event_Processing/BillionEventsPerOneHour/tests/testinputBevents.txt"
 HOST, PORT = "localhost", 9999
 data = "Hey Arunachala:whatsup man "
+
+def get_file_stream():
+    try:
+        file = open(PATH)
+    except:
+        print('Coundnt open the file', PATH)
+        sys.exit(1)
+    return file
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     sock.connect((HOST, PORT))
 
     while 1:
-        sock.sendall(bytes(data + "\n", "utf-8"))
-        print("Sent:     {}".format(data))
-        received = str(sock.recv(1024), "utf-8")
-        print("Received: {}".format(received))
-
-        time.sleep(1000)
-        exit(0)
-
-
-
-z00
+        file = get_file_stream()
+        while 1:   # Read all 1MM lines
+            line = file.readline()
+            print (line)
+            if line:
+                sock.send(bytes(line, "utf-8")),
+                print("Sent:     {}".format(line)),
+                #received = str(sock.recv(1024), "utf-8")
+                #print("Received: {}".format(received))
+            else:
+                break
--- server/server.py
+++ server/server.py
@@ -1,13 +1,16 @@
 import socketserver
 
+HOST, PORT = "localhost", 9999
+
 class MyTCPHandler(socketserver.StreamRequestHandler):
+
     def handle(self):
-        self.data = self.rfile.readline().strip()
-        print("{} wrote:".format(self.client_address[0]))
-        print(self.data)
-        self.wfile.write(self.data.upper())
+        while 1:
+            data = self.rfile.readline().strip()
+            # print("{} wrote:".format(self.client_address[0]))
+            print(data)
+            # self.wfile.write(self.data.upper())
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 9999
     with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
         server.serve_forever()
